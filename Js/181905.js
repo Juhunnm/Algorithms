c6c0370517aa0c4d97222a9f8846e312a70a92e1
juhun <minjuhun1@gmail.com>
2024-11-13 23:59:38 +0900

    11.13...

--- Js/181905.js
+++ Js/181905.js
@@ -3,7 +3,7 @@ function solution(my_string, s, e) {
     var answer = '';
     let arr = [...my_string.slice(s,e+1)].reverse()
     console.log(arr)
-    console.log(arr.splice(s,e+1,...arr));
+    console.log(arr.splice(s,arr.length,...arr));
     return answer;
 }
 
