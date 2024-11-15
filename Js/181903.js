27de06389b132f9970e8db8333a433af413de271
juhun <minjuhun1@gmail.com>
2024-11-15 23:43:20 +0900

    11.15 start

---
+++ Js/181903.js
@@ -0,0 +1,8 @@
+//qr code 
+function solution(q, r, code) {
+    var answer = '';
+    return answer;
+}
+
+
+console.log(solution(3,1,"qjnwezgrpirldywt"))
--- Js/181904.js
+++ Js/181904.js
@@ -1,13 +1,31 @@
 //세로 읽기
 function solution(my_string, m, c) {
-    var answer = '';
-    let arr=[];
-    for(let i =0;i<my_string.length;i++){
-        for(let j = 0; j<m;j++){
-            arr.push()
+    var answer = [...my_string];
+    let arr = [];
+    for (let i = 0; i < my_string.length / m; i++) {
+        arr[i] = [];
+        for (let j = 0; j < m; j++) {
+            arr[i].push(answer[i * m + j]);
         }
     }
+    answer = arr.map((i, idx) => i[c - 1]).join('')
     return answer;
 }
 
-console.log(solution("ihrhbakrfpndopljhygc",4,2))
+console.log(solution("ihrhbakrfpndopljhygc", 4, 2))
+//len 20
+
+// [
+//     [i,h,t,h], i =0 j =0
+//     [b,a,k,r], i= 1 j =
+//     [f,p,n,d],
+//     [o,p,l,j],
+//     [h,y,g,c]
+// ]
+
+// [
+//     'i', 'h', 'r', 'h', 'b', // 0 ~ 3 , 4(0(i) * 4(m) +0(j) ) 5(0 * 4 + 1) ~ 7 , 8 ~ 11
+//     'a', 'k', 'r', 'f', 'p',
+//     'n', 'd', 'o', 'p', 'l',
+//     'j', 'h', 'y', 'g', 'c'
+//   ]
