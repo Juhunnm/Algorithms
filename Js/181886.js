77f3a121143858fabaeea338314caa80cdc3f2b4
juhun <minjuhun1@gmail.com>
2024-11-23 23:56:11 +0900

    11.23

--- Js/181886.js
+++ Js/181886.js
@@ -1,6 +1,10 @@
 function solution(names) {
-    var answer = [];
+    var answer = []
+    for (let i = 0; i < names.length; i += 5) {
+        answer.push(names.slice(i, i+5).shift())
+        console.log(i)
+    }
     return answer;
 }
 
-console.log(solution["nami", "ahri", "jayce", "garen", "ivern", "vex", "jinx"])
+console.log(solution(["nami", "ahri", "jayce", "garen", "ivern", "vex", "jinx"]))
