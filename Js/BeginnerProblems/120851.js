1b7c94d441f6e4c86d9a79492ecd5ba96c01bd2d
juhun <minjuhun1@gmail.com>
2025-03-19 20:33:07 +0900

    120851.js reduce update

--- Js/BeginnerProblems/120851.js
+++ Js/BeginnerProblems/120851.js
@@ -1,8 +1,15 @@
+// const solution = (my_string) => {
+//     let answer = 0;
+//     [...my_string].filter((v) => !isNaN(v)).forEach(e => {
+//         answer+=Number(e)
+//     });;
+//     return answer
+// }
+
+// console.log(solution("aAb1B2cC34oOp"))
 const solution = (my_string) => {
     let answer = 0;
-    [...my_string].filter((v) => !isNaN(v)).forEach(e => {
-        answer+=Number(e)
-    });;
+    answer = [...my_string].filter((v) => !isNaN(v)).reduce((acc, cur) => acc + Number(cur),0)
     return answer
 }
 
