6014158f2dcf8561f6f73b2e119b9954bed0079d
juhun <minjuhun1@gmail.com>
2025-05-01 12:49:04 +0900

    120866 update

--- Js/Beginner/120866.js
+++ Js/Beginner/120866.js
@@ -1,22 +1,28 @@
 function solution(board) {
     var answer = 0;
-    const x = [-1, -1, -1, 0, 0, 1, 1, 1];
-    const y = [-1, 0, 1, -1, 1, -1, 0, 1]
-
+    let x = [-1, -1, -1, 0, 0, 1, 1, 1];
+    let y = [-1, 0, 1, -1, 1, -1, 0, 1]
     for (let i = 0; i < board.length; i++) {
         for (let j = 0; j < board[i].length; j++) {
             if (board[i][j] === 1) {
-                let tmp = [i, j]
                 for (let k = 0; k < 8; k++) {
-                    let nx = i + x[k];
-                    let ny = j + y
-                }
 
+                    let col = i + x[k];
+                    let row = j + y[k];
+                    if (col >= 0 && col < board.length &&
+                        row >= 0 && row < board[0].length
+                        && board[col][row] === 0) {
+                        board[col][row]=2;
+                    }
+                }
             }
         }
     }
-    console.log(board);
-
+    for(let i =0 ;i <board.length;i++){
+        for(let j =0 ;j<board[i].length;j++){
+            if(board[i][j] ===0) answer++;
+        }
+    }
 
     return answer;
 }
@@ -27,9 +33,8 @@ solution([
     [0, 0, 1, 0, 0],
     [0, 0, 0, 0, 0]
 ])
-
-//(3,2)x -1 ,+1 y = 자기 자신 뺴고는 -1,0,+1
-//
-//(2,1) ,(2,2), (2,3)
-//(3,1) , X ,  (3,3)
-//(4,1),(4,2),(4,3)
+// //(3,2)x -1 ,+1 y = 자기 자신 뺴고는 -1,0,+1
+// //
+// //(2,1) ,(2,2), (2,3)
+// //(3,1) , X ,  (3,3)
+// //(4,1),(4,2),(4,3)
